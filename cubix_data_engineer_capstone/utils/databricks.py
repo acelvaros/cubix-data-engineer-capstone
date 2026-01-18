aa7e0fde53acb7ef5591e239a1590f22985723ee
SZABGAB <gabor.szabo@allianz.com>
2026-01-18 18:55:00 +0100

    added minor changes and write file to volume

--- cubix_data_engineer_capstone/utils/databricks.py
+++ cubix_data_engineer_capstone/utils/databricks.py
@@ -1,3 +1,4 @@
+from more_itertools import partition
 from pyspark.sql import DataFrame, SparkSession
 
 def read_file_from_volume(full_path: str, format: str) -> DataFrame:
@@ -17,3 +18,32 @@ def read_file_from_volume(full_path: str, format: str) -> DataFrame:
         reader = reader.option("header", "true")
 
     return reader.load(full_path)
+
+
+def write_file_to_volume(
+    df: DataFrame,
+    full_path: str,
+    format: str,
+    mode: str= "overwrite",
+    partition_by: list[str] = None
+    ) -> None:
+    """Writes a DataFrame to UC Volume as parquet / csv / delta format.
+
+    :param df: DataFrame to be written.
+    :param file_path: The path to the file on the volume.
+    :param format: The format of the file ("csv", "json", "delta", "parquet").
+    :param mode: Default "overwrite", write mode.
+    :param partition_by: List of column to partition by, default is None.
+    """
+
+    if format not in ["csv", "delta", "parquet"]:
+        raise ValueError(f"Invalid format: {format}. Supported formats are: csv, parquet, delta.")
+    
+    writer = df.write.mode(mode).format(format)
+    if format == "csv":
+        writer = writer.option("header", True)
+    
+    if partition_by:
+        writer = writer.partitionBy(*partition_by)
+
+    writer.save(full_path)
