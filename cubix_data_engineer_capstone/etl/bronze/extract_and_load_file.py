7b0b737b8508d34a494eacce24d8916c8a787f03
SZABGAB <gabor.szabo@allianz.com>
2026-02-01 22:46:18 +0100

    commenting sdc1 and import STORAGE_ACCOUNT_NAME

--- cubix_data_engineer_capstone/etl/bronze/extract_and_load_file.py
+++ cubix_data_engineer_capstone/etl/bronze/extract_and_load_file.py
@@ -1,15 +1,44 @@
 from cubix_data_engineer_capstone.utils.databricks import read_file_from_volume, write_file_to_volume
 
-def bronze_ingest_volume(
-    source_path: str,
-    bronze_path: str,
-    file_name: str,
-    #partition_by: list[str]
-    ):
+def bronze_ingest(
+        source_path: str,
+        bronze_path: str,
+        file_name: str,
+        container_name: str,
+        partition_by: list[str] = None
+):
     """Extract files from the source, and load them to the desired container.
-    :param source_path: Path to source file.
-    :param bronze_path: Path to the bronze layer.
-    :param partition_by: Column(s) to partition on. "None" by default.
+
+    :param source_path:                 Path to source file.
+    :param bronze_path:                 Path to the bronze layer.
+    :param file_name:                   Name of the file to ingest.
+    :param container_name:              Name of the container holding the files.
+    :param partition_by:                Column(s) to partition on. "None" by default.
+    """
+    df = read_file_from_volume(container_name, f"{source_path}/{file_name}", "csv")
+
+    return write_file_to_volume(
+        df=df,
+        container_name=container_name,
+        file_path=f"{bronze_path}/{file_name}",
+        format="csv",
+        mode="overwrite",
+        partition_by=partition_by
+    )
+
+
+def bronze_ingest_volume(
+        source_path: str,
+        bronze_path: str,
+        file_name: str,
+        partition_by: list[str] = None,
+):
+    """Extract files from the source, and load them to the Bronze volume.
+
+    :param source_path:                 Path to source file.
+    :param bronze_path:                 Path to the bronze layer.
+    :param file_name:                   Name of the file to ingest.
+    :param partition_by:                Column(s) to partition on. "None" by default.
     """
     df = read_file_from_volume(f"{source_path}/{file_name}", "csv")
 
@@ -17,5 +46,6 @@ def bronze_ingest_volume(
         df=df,
         full_path=f"{bronze_path}/{file_name}",
         format="csv",
-        mode="overwrite"
+        mode="overwrite",
+        partition_by=partition_by
     )
--- cubix_data_engineer_capstone/etl/silver/scd.py
+++ cubix_data_engineer_capstone/etl/silver/scd.py
@@ -1,19 +1,20 @@
 from delta.tables import DeltaTable     # type: ignore
 from pyspark.sql import DataFrame, SparkSession
 
+"""
 from cubix_data_engineer_capstone.utils.config import STORAGE_ACCOUNT_NAME
 
-
 def scd1(spark: SparkSession, container_name: str, file_path: str, new_data: DataFrame, primary_key: str):
-    """Slowly Changing Dimension Type 1 implementation. Compare the master Delta table with the "new_data",
-    if there are changes on the given primary key, then update, if it's not found in master, then insert.
-
-    :param spark:           SparkSession.
-    :param container_name:  Name of the container holding the Delta table.
-    :param file_path:       Path to the Delta table.
-    :param new_data:        DataFrame with the new data.
-    :param primary_key:     Primary Key to join the Master Delta table with the new data.
-    """
+    #
+    #Slowly Changing Dimension Type 1 implementation. Compare the master Delta table with the "new_data",
+    #if there are changes on the given primary key, then update, if it's not found in master, then insert.
+    #
+    #:param spark:           SparkSession.
+    #:param container_name:  Name of the container holding the Delta table.
+    #:param file_path:       Path to the Delta table.
+    #:param new_data:        DataFrame with the new data.
+    #:param primary_key:     Primary Key to join the Master Delta table with the new data.
+    #
     master_path = f"abfss://{container_name}@{STORAGE_ACCOUNT_NAME}.dfs.core.windows.net/{file_path}"
     delta_master = DeltaTable.forPath(spark, master_path)
 
@@ -28,7 +29,7 @@ def scd1(spark: SparkSession, container_name: str, file_path: str, new_data: Dat
         .whenNotMatchedInsertAll()
         .execute()
     )
-
+"""
 
 def scd1_uc(spark: SparkSession, table_name: str, new_data: DataFrame, primary_key: str):
     """
